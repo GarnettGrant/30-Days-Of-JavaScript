3d882be40578ad8814e21d667dc20fdd5e474ef7
GarnettGrant <net_grant@hotmail.com>
2022-04-28 22:58:25 -0400

    Updated

--- 02_Day_Data_types/excerciseLvl3.js
+++ 02_Day_Data_types/excerciseLvl3.js
@@ -8,7 +8,10 @@ q2.match(/because/gi).length; // returns 3
 
 //3. Clean the following text and find the most frequent word (hint, use replace and regular expressions).
 const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
-sentence.replace(/[$%@&#]/g,'');
+let cleanSentence = sentence.replace(/[$%@&#;]/g,''); //returns sentence.replace(/[$%@&#;]/g,''); 'I am a teacher, and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher!? This 30DaysOfJavaScript is also the result of love of teaching' 
+//Most frequent word: 
+cleanSentence.split(' ');
+
 
 //4. Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
 let q4 = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.';
