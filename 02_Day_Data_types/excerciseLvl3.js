571976cd643355b83a6788f9cbf85c3819064dd2
GarnettGrant <net_grant@hotmail.com>
2022-04-29 22:17:56 -0400

    Incomplete

--- 02_Day_Data_types/excerciseLvl3.js
+++ 02_Day_Data_types/excerciseLvl3.js
@@ -10,7 +10,7 @@ q2.match(/because/gi).length; // returns 3
 const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
 let cleanSentence = sentence.replace(/[$%@&#;]/g,''); //returns sentence.replace(/[$%@&#;]/g,''); 'I am a teacher, and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher!? This 30DaysOfJavaScript is also the result of love of teaching' 
 //Most frequent word: 
-cleanSentence.split(' ');
+cleanSentence.split(/\W+/g); //Updated to use regex
 
 
 //4. Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
